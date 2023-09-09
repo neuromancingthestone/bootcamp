ce52c48c587164c7f40af50b6b4d562f16b26501
neuromancingthestone <144539215+neuromancingthestone@users.noreply.github.com>
2023-09-09 09:21:01 -0400

    Update timers.js

--- First Project/timers.js
+++ First Project/timers.js
@@ -1,1 +1,24 @@
+function countDown(x) {
+    let intervalID = setInterval(function(){
+        x--;
+        console.log(x);
+        if( x == 0) {
+            console.log("DONE");
+            clearInterval(intervalID); 
+        }
+    }, 1000); 
+}
 
+function randomGame() {
+    let count = 1;
+    let intervalID = setInterval(function(){
+        let randomNum = Math.random();
+ //       console.log(randomNum);        
+        if(randomNum > .75) {
+            console.log(count);
+            clearInterval(intervalID);            
+        } else {
+            count++;
+        }
+    }, 1000)
+}
